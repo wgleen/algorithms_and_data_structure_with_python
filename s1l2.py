f79a9beb53013b198f4b7dc001785f835a5d7323
Wallace Gleen <wallace.gleen@gmail.com>
2019-03-17 17:16:51 -0300

    s1l2

---
+++ s1l2.py
@@ -0,0 +1,47 @@
+'''
+Python basic syntax.
+'''
+
+print('Hello world')
+
+var = 10
+
+print(var)
+
+print(type(var))
+
+var = 'Test'
+
+print(type(var))
+print(var.upper())
+print(var.lower())
+
+var = None
+
+print(type(var))
+
+var = 3.14
+
+print(type(var))
+
+arr = ['john', 1, 3.14, None]
+
+print(arr[0])
+print(arr[-1])
+print(arr[:2])
+
+tul = (1, 2, 3)
+
+print(tul[0])
+print(tul[-1])
+print(tul[:1])
+
+name = 'Wallace'
+
+print(name[0])
+
+dic = {
+    'Wallace': 27
+}
+
+print(dic['Wallace'])
